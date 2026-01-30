46f2431663d8f3eb4c501971d669e3281dd22f87
Heescheo <Heescheo@gmail.com>
2026-01-30 14:06:54 +0100

    Hjemmeopgave01 med 2 forskellige svar på hjemmeopgave.

---
+++ src/Hjemmeopgaver/HjemmeOpgave01_VariableStringWriteToConsole.java
@@ -0,0 +1,16 @@
+package Hjemmeopgaver;
+
+public class HjemmeOpgave01_VariableStringWriteToConsole {
+    public static void Message03() {
+
+
+        //Using the string variable together with \t to get the wanted spaces.
+        String Mission = "Mission \t Year \t Days in Space";
+        String IRISS = "IRISS \t \t 2015 \t 10";
+        String Huginn = "Huginn \t \t 2023 \t 180";
+        //The value of these variables is written to the consol. Using println to get new line.
+        System.out.println(Mission);
+        System.out.println(IRISS);
+        System.out.println(Huginn);
+    }
+}
